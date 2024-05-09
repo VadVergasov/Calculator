99411be24ddbf5c5671da75852850958093afc34
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2024-05-09 17:29:31 +0300

    Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.vadvergasov.calculator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         signingConfig = signingConfigs.getByName("debug")
